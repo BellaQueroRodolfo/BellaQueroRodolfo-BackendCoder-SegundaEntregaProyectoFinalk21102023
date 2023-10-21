41ccffc38e4a0e8e34466c56468c7f62c50c8510
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-10-21 19:13:04 -0300

    Update product.js

--- BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023-main/BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023/src/routes/product.js
+++ BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023-main/BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023/src/routes/product.js
@@ -1,36 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../dao/models/Product');
+const ProductManager = require('../dao/managers/productManager');
+const productManager = new ProductManager();
 
 router.get('/', async (req, res) => {
   try {
-    const products = await Product.find();
-    res.json({ products });
+    const { limit = 10, page = 1, sort, query } = req.query;
+    const result = await productManager.getProducts({
+      limit: parseInt(limit),
+      page: parseInt(page),
+      sort,
+      query,
+    });
+    res.json(result);
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-router.get('/:pid', async (req, res) => {
-  const productId = req.params.pid;
+router.get('/:id', async (req, res) => {
   try {
-    const product = await Product.findById(productId);
+    const productId = req.params.id;
+    const product = await productManager.getProductById(productId);
     if (!product) {
       res.status(404).json({ error: 'Product not found' });
     } else {
-      res.json({ product });
+      res.json(product);
     }
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    const { title, description, price } = req.body;
-    const newProduct = new Product({ title, description, price });
-    await newProduct.save();
-    res.status(201).json({ product: newProduct });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
