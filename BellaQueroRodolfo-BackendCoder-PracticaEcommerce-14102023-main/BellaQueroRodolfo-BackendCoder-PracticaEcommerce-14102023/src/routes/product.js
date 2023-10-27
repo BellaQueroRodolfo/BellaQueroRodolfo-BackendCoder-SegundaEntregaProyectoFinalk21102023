3adfed6f5b3bb4c0129db9e3b3e1ed88ded8fdc1
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-10-27 18:42:38 -0300

    Update product.js

--- BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023-main/BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023/src/routes/product.js
+++ BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023-main/BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023/src/routes/product.js
@@ -1,33 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const ProductManager = require('../dao/managers/productManager');
-const productManager = new ProductManager();
+const Product = require('../models/Product');
 
 router.get('/', async (req, res) => {
   try {
     const { limit = 10, page = 1, sort, query } = req.query;
-    const result = await productManager.getProducts({
-      limit: parseInt(limit),
-      page: parseInt(page),
-      sort,
-      query,
-    });
-    res.json(result);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
 
-router.get('/:id', async (req, res) => {
-  try {
-    const productId = req.params.id;
-    const product = await productManager.getProductById(productId);
-    if (!product) {
-      res.status(404).json({ error: 'Product not found' });
-    } else {
-      res.json(product);
-    }
-  } catch (error) {
+    const options = {
+      page: parseInt(page),
+      limit: parseInt(limit),
+      sort: sort === 'desc' ? '-price' : 'price',
+    };
+
+    const filter = query ? { category: query } : {};
+
+    const products = await Product.paginate(filter, options);
+    res.json(products);
+  } catch (err) {
+    console.error(err);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
